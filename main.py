56dec20d3d15ad45343b1ef49945f538988467cf
theojl6 <68571283+theojl6@users.noreply.github.com>
2021-09-03 11:00:54 +0800

    add get_total_page feature

--- main.py
+++ main.py
@@ -4,19 +4,34 @@ from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 import io
 
+"""TODO: add feature to choose pages"""
+
+# URL of a WebPage
+URL = "https://dlpsgame.org/category/ps3/"
+# use HEADERS to avoid "405 METHOD NOT ALLOWED" error
+HEADERS = {"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0"}
+
+"""Gets the latest page number from HTML page source"""
+def get_total_page():
+  req = Request(url=URL, headers=HEADERS)
+  html = urlopen(req).read()
+  soup = BeautifulSoup(html, 'html.parser')
+  tag = soup.find(class_="pages")
+  pages = tag.string
+  pages = pages.split(" ")[-1]
+  return int(pages)
+
+
+
 if __name__ == "__main__":
 
-  # URL of a WebPage
-  url = "https://dlpsgame.org/category/ps3/"
-  # use HEADERS to avoid "405 METHOD NOT ALLOWED" error
-  HEADERS = {"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0"} 
-
+  last_page = get_total_page()
   errors = []
-  with io.open("output.txt", "w", encoding="utf-8") as output:
-    for page_number in range(1, 166): # Loop over all pages currently, TODO: find maximum page number from HTML page source
+  with io.open("output.txt", "w", encoding="utf-8") as output: # Use io.open() with encoding="utf-8" to avoid error
+    for page_number in range(1, last_page + 1):
       # Open the URL and read the whole page
       try:
-        req = Request(url=url + f"page/{page_number}/", headers=HEADERS)
+        req = Request(url=URL + f"page/{page_number}/", headers=HEADERS)
         html = urlopen(req).read()
         # Parse the string
         soup = BeautifulSoup(html, 'html.parser')
